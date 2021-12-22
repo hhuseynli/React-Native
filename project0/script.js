ccda6f6bd9167047516b416fc3c1fcd0b681e17f
Huseyn <58394849+hhuseynli@users.noreply.github.com>
2021-12-22 09:05:34 +0400

    Project 0 Done

--- project0/script.js
+++ project0/script.js
@@ -9,6 +9,44 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
-function newTodo() {
-  alert('New TODO button clicked!')
+let id = 0
+let todos = []
+
+function renderTodo(id){
+  console.log(id)
+  const li = document.createElement('li')
+  li.className = classNames.TODO_ITEM
+  li.innerHTML = ` 
+  <input type="checkbox" ${todos[id].checked ? "checked": ""}
+  class="${classNames.TODO_CHECKBOX}"  onchange="changeSelection(${id})"/> 
+  <span class="${classNames.TODO_TEXT}">
+  ${todos[id].text}</span> 
+  
+  `
+  //<button class=${classNames.TODO_DELETE} onclick="deleteTodo(${todos[id]})"/>Delete Todo</button>
+  return li
 }
+
+function render(){
+  list.innerHTML = ""
+  itemCountSpan.innerHTML = todos.length
+  uncheckedCountSpan.innerHTML = todos.filter(todo => !todo.checked).length
+  todos.map((todo, index) => renderTodo(index)).forEach(todo => {list.appendChild(todo)})
+}
+
+function newTodo() {
+  const text = prompt("Enter your todo")
+  todos.push({id:id++, text:text, checked:false}) 
+  return render()
+}
+
+function changeSelection(id) {
+  todos[id].checked = !todos[id].checked
+  return render()
+}
+
+// function deleteTodo(todo) {
+//   todos = todos.filter(t => t !== todo)
+//   return render()
+// }
+
