117e0de9188e54a8f24327b9c5202e550e96c439
Huseyn <58394849+hhuseynli@users.noreply.github.com>
2021-12-25 11:17:43 +0400

    pomodoro timer v1
    this version only contains 25 min countdown and pause/stop button

--- project1/App.js
+++ project1/App.js
@@ -1,13 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, Vibration} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    minutes: 25,
+    seconds: 0,
+    isStarted: true
+
+  }
+
+  componentDidMount() {
+    this.checkTime()
+    if(this.state.isStarted){
+      this.decreaseSecs()
+    }
+  } 
+
+  componentDidUpdate() {
+    this.checkTime()
+  }
+    
+  checkTime(){
+    if(this.state.minutes === 0 && this.state.seconds === 0 && this.state.isStarted){
+      this.stopTimer()
+      Vibration.vibrate()
+    } else if (this.state.seconds === 0 && this.state.isStarted){
+      this.setState(prevState => ({
+        minutes: prevState.minutes - 1,
+        seconds: 59
+      }));
+  }
+}
+  
+  formatTime = (min, sec) => {
+    return min + ":" + (sec.toString().length > 1 ? sec : "0" + sec);
+  }
+
+  stopTimer = () => {
+    this.setState({isStarted: false});
+    if(this.interval) clearInterval(this.interval)
+  }
+
+  pauseTimer = () => {
+    this.decreaseSecs()
+    this.setState({isStarted:true})    
+  }
+
+  decreaseSecs() {
+      this.interval = setInterval(() => this.setState(prevState => ({
+        seconds: prevState.seconds - 1
+      })), 1000)    
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}> Work Timer </Text>
+        <Text style={styles.time}>
+          {this.formatTime(this.state.minutes, this.state.seconds)}
+        </Text>
+        <Button 
+        title={this.state.isStarted ? "Pause" : "Start"} 
+        onPress={this.state.isStarted ? this.stopTimer : this.pauseTimer} 
+        />
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +76,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 48,
+    fontWeight: 'bold'
+  },
+  time: {
+    fontSize: 42
+  }
 });
