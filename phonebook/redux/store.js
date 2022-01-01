bd4abf9df550966b648327ab8564a059f992ee35
Huseyn <58394849+hhuseynli@users.noreply.github.com>
2022-01-01 12:12:53 +0400

    Update store.js
    changing numbers so they are not the same

--- phonebook/redux/store.js
+++ phonebook/redux/store.js
@@ -4,7 +4,7 @@ const { reducer } = require('./reducers')
 
 
 const store = createStore(reducer)
-store.dispatch(addContact({name:"Jordan Hayashi", phone:"0123456789"}))
-store.dispatch(addContact({name:"David J. Malan", phone:"0123456789"}))
-store.dispatch(addContact({name:"Huseyn Huseynli", phone:"0123456789"}))
+store.dispatch(addContact({name:"Jordan Hayashi", phone:"0123456252"}))
+store.dispatch(addContact({name:"David J. Malan", phone:"0123453527"}))
+store.dispatch(addContact({name:"Huseyn Huseynli", phone:"0123226789"}))
 export default store
